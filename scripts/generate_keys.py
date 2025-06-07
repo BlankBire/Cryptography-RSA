7cde2d950633bc4bd190eedea37d635dd78139e0
BlankBire <nguyenhuucanhnc2005@gmail.com>
2025-06-07 17:22:26 +0700

    clean project

--- scripts/generate_keys.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
-from keygen import generate_rsa_keypair
-
-if __name__ == "__main__":
-    print("[🔑] Generating RSA key pair (2048 bits)...")
-    generate_rsa_keypair(3072)
